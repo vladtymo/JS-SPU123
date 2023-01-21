d1ebd20c9a2c2bd0966345ccc426eabf8636b995
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-01-21 13:13:05 +0200

    first commit

---
+++ 01_Intro/main.js
@@ -0,0 +1,39 @@
+// ------- data types
+
+let koef = 10.55;
+let login = "super_user123";
+let flag = true;
+let summ = function (a, b) { console.log(a + b); };
+let user = {
+    login: "super_programmer",
+    password: "Qwerty"
+};
+let array = [1, 44, -4, "red", "green", true, [1, 2, 3], user, summ];
+
+// interpolation `...${value}...`
+// '' equals to ""
+
+// show variable types
+console.log(`Number: ${koef}, type: ${typeof (koef)}`);
+console.log(`Login: ${login}, type: ${typeof (login)}`);
+console.log(`Flag: ${flag}, type: ${typeof (flag)}`);
+console.log(`User: ${user.login} ****, type: ${typeof (user)}`);
+console.log(`Function: ${summ}, type: ${typeof (summ)}`);
+console.log(`Array: ${array}, type: ${typeof (array)}`);
+
+// dynamic typing: we can change types in runtime
+koef = 'A';
+console.log(`Number: ${koef}, type: ${typeof (koef)}`);
+
+// ------- browser dialogs
+alert("Some alert message!");
+username = prompt("Enter your name:");
+
+// convert string to number: number = +{string}
+age = +prompt("Enter your age:");
+
+console.log(`Username: ${username}, type: ${typeof (username)}`);
+console.log(`Age: ${age + 1}, type: ${typeof (age)}`);
+
+// step: value ** step
+console.log('Step: ${2 ** 8}');
