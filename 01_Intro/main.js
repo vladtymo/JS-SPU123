990fae9d6ecbb48a254ebe361bd22abfe65f2af2
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-01-28 13:26:20 +0200

    Array in JS example.

--- 01_Intro/main.js
+++ 01_Intro/main.js
@@ -3,7 +3,10 @@
 let koef = 10.55;
 let login = "super_user123";
 let flag = true;
+// anonymous function
 let summ = function (a, b) { console.log(a + b); };
+// arrow function
+let div = (a, b) => console.log(a + b);
 let user = {
     login: "super_programmer",
     password: "Qwerty"
@@ -37,3 +40,6 @@ console.log(`Age: ${age + 1}, type: ${typeof (age)}`);
 
 // step: value ** step
 console.log('Step: ${2 ** 8}');
+
+let isContinue = confirm("Do you want to continue?");
+alert(isContinue ? "Good!" : "Exit!");
