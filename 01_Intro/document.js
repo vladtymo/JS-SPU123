7c608f9b7925428fa456d90ca7e4e1efab5f77de
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-02-11 13:16:42 +0200

    Objects + intro to DOM.

---
+++ 01_Intro/document.js
@@ -0,0 +1,12 @@
+// -------------- document --------------
+const username = prompt("Enter your name:");
+
+// 1 - using document methods
+let par = document.createElement("p");
+par.innerText = `Hello, dear: ${username}`;
+par.style.color = 'darkcyan';
+
+document.body.appendChild(par);
+
+// 2 - using document write
+//document.write(`<p style='color: darkcyan;'>Hello, dear: ${username}</p>`);
